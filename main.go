fe145147ff56665828e6c0bfd9311e6795266127
배나영 <baenayoung@baenayoung-ui-MacBook-Pro.local>
2022-03-14 14:59:56 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nayoung511/blockchain_build/blockchain"
+
+)
+
+func main(){
+	chain := blockchain.InitBlockChain()
+
+	chain.AddBlock("First block after Genesis")
+	chain.AddBlock("Second block after Genesis")
+	chain.AddBlock("Third block after Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+		fmt.Println()
+	}
+
+}
